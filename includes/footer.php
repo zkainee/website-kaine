8f69690fb7e58db1284286047a154854ed1d87ed
Kaine <kainevommeren@gmail.com>
2023-01-15 20:27:18 +0100

    commit 2

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+<?php
+// Year count every year
+$year = date("Y")
+?>
+    <div class="container">
+        <footer class="py-3 my-4">
+            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                <li class="nav-item"><a href="/website-kaine/pages/about-me.php" class="nav-link px-2 text-muted">Over mij</a></li>
+                <li class="nav-item"><a href="/website-kaine/pages/portfolio.php" class="nav-link px-2 text-muted">Portfolio</a></li>
+                <li class="nav-item"><a href="/website-kaine/pages/contact.php" class="nav-link px-2 text-muted">Contact</a></li>
+            </ul>
+            <p class="text-center text-muted">© Kaine <?php echo $year ?></p>
+        </footer>
+    </div>
+</html>
---
+++ pages/about-me.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="nl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="icon" href="./img/icon-title.png">
+        <link href="../css/bootstrap.css" rel="stylesheet">
+        <script rel="script" src="../js/bootstrap.js" defer></script>
+        <link href="../css/bootstrap-utilities.css" rel="stylesheet" >
+        <title>Website | Kaine</title>
+    </head>
+    <body class="bg-light">
+        <nav class="nav m-3">
+            <!-- NAVIGATION -->
+            <div class="container">
+                <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="140" height="140" viewBox="0 0 145 134">
+                        <image id="logo" data-name="logo" width="100" height="100" href="#"></image>
+                    </svg>
+                    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                        <li><a href="../index.php" class="nav-link px-2 link-secondary">Home</a></li>
+                        <li><a href="/website-kaine/pages/about-me.php" class="nav-link px-2 link-dark">Over mij</a></li>
+                        <li><a href="/website-kaine/pages/portfolio.php" class="nav-link px-2 link-dark">Portfolio</a></li>
+                        <li><a href="/website-kaine/pages/contact.php" class="nav-link px-2 link-dark">Contact</a></li>
+                    </ul>
+                </header>
+            </div>
+        </nav>
+
+    </body>
+<?php include "../includes/footer.php" ?>
